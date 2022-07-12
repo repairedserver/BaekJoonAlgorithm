9f3609eaa0e3218ce9a3fc0096886c742b99f9a7
Kim sehyun <dnjfdid14@naver.com>
2022-07-12 18:08:18 +0900

    손익분기점

---
+++ 백준1712.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int gibon, suik, biyong;
+
+	scanf("%d %d %d", &gibon, &suik, &biyong);
+
+	if (suik >= biyong) printf("-1\n");
+	else printf("%d\n", gibon / (biyong - suik) + 1);
+	return 0;
+}
