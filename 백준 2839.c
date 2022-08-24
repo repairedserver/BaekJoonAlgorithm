a7f8a32f6cc5cc864d9d53139bc765f8503c2b51
Kim sehyun <dnjfdid14@naver.com>
2022-08-24 16:43:32 +0900

    설탕 배달

---
+++ 백준 2839.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+	int n;
+	int count = 0;
+	scanf("%d", &n);
+	while (n > 0) {
+		if (n % 5 == 0) {
+			n -= 5;
+			count++;
+		}
+		else if (n % 3 == 0) {
+			n -= 3;
+			count++;
+
+		}
+		else if (n > 5) {
+			n -= 5;
+			count++;
+
+		}
+		else {
+			count = -1;
+			break;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
