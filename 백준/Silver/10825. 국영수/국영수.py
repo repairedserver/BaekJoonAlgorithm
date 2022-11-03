98cc028d902b412e0842aca32a189ed8e7ce9868
김세현 <101509164+repairedserver@users.noreply.github.com>
2022-11-04 08:10:18 +0900

    [Silver IV] Title: 국영수, Time: 4440 ms, Memory: 61856 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = []
+for i in range(n):
+    a,b,c,d = list(map(str, input().split()))
+    arr.append([a,int(b),int(c),int(d)])
+    
+arr.sort(key = lambda i : (-i[1] , i[2],-i[3],i[0]))
+for i in arr:
+    print(i[0])
