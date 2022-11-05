363278d418352e5d1be0ad3c0b712c4464de7222
김세현 <101509164+repairedserver@users.noreply.github.com>
2022-11-05 12:56:32 +0900

    [Silver IV] Title: 문자열, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -0,0 +1,10 @@
+a, b = map(str, input().split())
+res = []
+for i in range(len(b) - len(a) + 1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            cnt += 1
+    res.append(cnt)
+
+print(min(res))
