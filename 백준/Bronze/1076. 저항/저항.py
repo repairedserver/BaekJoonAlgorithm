6216a94f13f28487c8de7f9fd9145d8c1bb2496e
김세현 <101509164+repairedserver@users.noreply.github.com>
2022-12-26 12:44:00 +0900

    [Bronze II] Title: 저항, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,6 @@
+c = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+c1 = str(c.index(input()))
+c2 = str(c.index(input()))
+c3 = str(10**c.index(input()))
+
+print(int(c1+c2+c3[1:]))
