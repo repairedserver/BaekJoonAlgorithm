d0ab19850ffd8e3ca9ad6b2430de6b2523608bd9
김세현 <101509164+repairedserver@users.noreply.github.com>
2023-04-10 19:29:08 +0900

    [Silver II] Title: -2진수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2089. －2진수/－2진수.py
@@ -0,0 +1,13 @@
+n = int(input())
+ans = ''
+if n == 0:
+    print(0)
+    exit()
+while n:
+    if n % (-2):
+        ans = '1' + ans
+        n = n // -2 + 1
+    else:
+        ans = '0' + ans
+        n = n // -2
+print(ans)
