e531322fb9a2c0fa160712a05fcf6f38bb78ec2a
김세현 <101509164+repairedserver@users.noreply.github.com>
2023-04-12 17:31:38 +0900

    [Silver III] Title: 소수 구하기, Time: 180 ms, Memory: 41468 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,15 @@
-a, b = map(int, input().split())
+m, n = map(int, input().split())
+def find_p(n, m):
+    s = [True] * (m + 1)
+    s[0] = s[1] = False
 
-for i in range(a, b+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i ** 0.5)+1):
-        if i%j==0:
-            break
-    else:
-        print(i)
+    for i in range(2, int(m ** 0.5) + 1):
+        if s[i]:
+            for j in range(i * i, m + 1, i):
+                s[j] = False
+
+    return [j for j in range(n, m + 1) if s[j]]
+
+p = find_p(m, n)
+for i in range(len(p)):
+    print(p[i])
