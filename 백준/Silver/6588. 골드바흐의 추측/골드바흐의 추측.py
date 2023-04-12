585109fcd44289131863c9f55c5cfb6ca45a401a
김세현 <101509164+repairedserver@users.noreply.github.com>
2023-04-12 19:57:58 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1812 ms, Memory: 46884 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,16 +1,19 @@
 import sys
 input = sys.stdin.readline
-arr = [False, False] + [True] * 1000000
-for i in range(2, 1001):
-    if arr[i] == True:
-        for j in range(i + i, 1000001, i):
-            arr[j] = False
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    for i in range(3, n + 1, 2):
-        if arr[i] and arr[n-i]:
-            print(f'{n} = {i} + {n-i}')
+def yee():
+    arr = [False, False] + [True] * 1000000
+    for i in range(2, 1001):
+        if arr[i] == True:
+            for j in range(i + i, 1000001, i):
+                arr[j] = False
+    while True:
+        n = int(input())
+        if n == 0:
             break
-        i += 2
+        for i in range(2, 1000001):
+            if arr[i] and arr[n-i]:
+                print(f'{n} = {i} + {n-i}')
+                break
+        else:
+            print("Goldbach's conjecture is wrong.")
+yee()
