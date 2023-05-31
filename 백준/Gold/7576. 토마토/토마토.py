d82408ad7314e7593a62ae262b585072c4833419
김세현 <101509164+repairedserver@users.noreply.github.com>
2023-05-31 17:22:23 +0900

    [Gold V] Title: 토마토, Time: 1376 ms, Memory: 106704 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,29 +1,33 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
+
 m, n = map(int, input().split())
-mat = [list(map(int, input().split())) for _ in range(n)]
+a = [list(map(int, input().split())) for i in range(n)]
 q = deque([])
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-res = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+ans = 0
 
-for i in range(n):
-    for j in range(m):
-        if mat[i][j] == 1:
-            q.append([i, j])
-
-def yee():
+def bfs():
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx, ny = dx[i] + x, dy[i] + y
-            if 0 <= nx < n and 0 <= ny < m and mat[nx][ny] == 0:
-                mat[nx][ny] = mat[x][y] + 1
+            if 0 <= nx < n and 0 <= ny < m and a[nx][ny] == 0:
+                a[nx][ny] = a[x][y] + 1
                 q.append([nx, ny])
 
-yee()
-for i in mat:
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == 1:
+            q.append([i, j])
+
+bfs()
+for i in a:
     for j in i:
         if j == 0:
             print(-1)
             exit()
-    res = max(res, max(i))
-print(res - 1)
+    ans = max(ans, max(i))
+print(ans - 1)
