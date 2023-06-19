daa4a38955f963815e48d980ca9ad0cc86b4b60e
김세현 <101509164+repairedserver@users.noreply.github.com>
2023-06-19 20:26:07 +0900

    [Gold II] Title: 트리의 지름, Time: 5856 ms, Memory: 74560 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(99999)
+n = int(input())
+tree = [[] for i in range(n+1)]
+visited = [-1] * (n+1)
+visited[1] = 0
+idx = 0
+tmp = 0
+
+def dfs(node, cost):
+    for i, j in tree[node]:
+        cal_w = cost + j
+        if visited[i] == -1:
+            visited[i] = cal_w
+            dfs(i, cal_w)
+    return
+
+for i in range(n):
+    w = list(map(int, input().split()))
+    for j in range(1, len(w)-2, 2):
+        tree[w[0]].append([w[j], w[j+1]])
+
+visited = [-1] * (n+1)
+visited[1] = 0
+
+dfs(1, 0) 
+s = visited.index(max(visited))
+
+visited = [-1] * (n+1)
+visited[s] = 0
+dfs(s, 0)
+print(max(visited))
