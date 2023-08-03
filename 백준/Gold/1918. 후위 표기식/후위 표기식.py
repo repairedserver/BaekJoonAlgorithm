585d5e65cd5bb241a02617ae5f9cc3747690c0df
김세현 <101509164+repairedserver@users.noreply.github.com>
2023-08-03 09:26:27 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,25 @@
+l = list(input())
+st = []
+ans = ''
+
+for i in l:
+    if i.isalpha():
+        ans += i
+    else:
+        if i == '(':
+            st.append(i)
+        elif i == '*' or i == '/':
+            while st and (st[-1] == '*' or st[-1] =='/'):
+                ans += st.pop()
+            st.append(i)
+        elif i == '+' or i == '-':
+            while st and st[-1] != '(':
+                ans+= st.pop()
+            st.append(i)
+        elif i == ')':
+            while st and st[-1] != '(':
+                ans += st.pop()
+            st.pop()
+while st:
+    ans += st.pop()
+print(ans)
