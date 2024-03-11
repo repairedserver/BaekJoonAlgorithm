93a2d8b7f4e4cdd01da53c480784fd460d954711
김세현 <101509164+repairedserver@users.noreply.github.com>
2024-03-11 12:18:23 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,13 +1,9 @@
-from collections import deque
 n, m = map(int, input().split())
-
-peo = deque()
-for i in range(1, n+1): peo.append(i)
-ans = []
-
-while peo:
-    for _ in range(m-1):
-        peo.append(peo.popleft())
-    ans.append(peo.popleft())
-
-print(str(ans).replace('[', '<').replace(']', '>'))
+a = [i for i in range(1, n+1)]
+now = -1
+print("<", end="")
+while len(a) != 1:
+    now = (now+m) % len(a)
+    print(a.pop(now), end=", ")
+    now-=1
+print(a.pop(), end=">")
