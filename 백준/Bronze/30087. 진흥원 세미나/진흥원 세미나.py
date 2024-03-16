20321d5a006f6242729cefc88650c1d3258b27a0
김세현 <101509164+repairedserver@users.noreply.github.com>
2024-03-16 15:10:06 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,10 @@
+d = {"Algorithm": "204", 
+     "DataAnalysis": "207", 
+     "ArtificialIntelligence": "302", 
+     "CyberSecurity": "B101", 
+     "Network": "303", 
+     "Startup": "501", 
+     "TestStrategy": "105"}
+s = [input() for i in range(int(input()))]
+for i in s:
+    print(d[i])
