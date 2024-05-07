494deccecd77627e4e3c6b57501d1be90486342b
김세현 <101509164+repairedserver@users.noreply.github.com>
2024-05-07 15:42:11 +0900

    [Silver II] Title: 랜선 자르기, Time: 68 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,16 +1,26 @@
-k, n = map(int, input().split())
-lan = [int(input()) for i in range(k)]
+import sys
+input = sys.stdin.readline
 
-ans = 0
-st, end = 1, max(lan)
-while st <= end:
-    mid = (st + end) // 2
-    temp_sum = 0
-    for i in lan:
-        temp_sum += i // mid
-    if temp_sum >= n:
-        st = mid + 1
-        ans = mid
+def binary_search(low, high):
+    global n
+    global ans
+    if high < low:
+        return
+    mid = (low+high) // 2
+    t = 0
+    for i in l:
+        t += i//mid
+    if t >= n:
+        ans = mid   
+        binary_search(mid+1, high)
     else:
-        end = mid - 1
+        binary_search(low, mid-1)
+        
+k, n = map(int, input().split())
+l = []
+for i in range(k):
+    l.append(int(input()))
+l_max = max(l)
+ans = 0
+binary_search(0, l_max*2)
 print(ans)
