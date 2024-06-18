878e7d80821208d6c01edb986b25cf29977bf56d
김세현 <101509164+repairedserver@users.noreply.github.com>
2024-06-18 13:45:35 +0900

    [level 2] Title: 더 맵게, Time: 1094.96 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 67.1 MB, 시간: 1238.57 ms
+메모리: 67.2 MB, 시간: 1094.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 17:26:3
+2024년 06월 18일 13:45:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,17 +1,13 @@
 from collections import deque
-
 def solution(scoville, K):
     ans = 0 
     d = deque()
     scoville.sort()
-    s = deque(i for i in scoville)
-    
+    s = deque(i for i in scoville) 
     while (s and s[0] < K) or (d and d[0] < K):
         ans += 1
-        
         if len(s) + len(d) <= 1:
             return -1
-        
         a = [0] * 2
         for i in range(2):
             if s and d:
@@ -23,7 +19,5 @@ def solution(scoville, K):
                 a[i] = s.popleft()
             else:
                 a[i] = d.popleft()
-            
         d.append(a[0] + a[1]*2)
-        
     return ans
